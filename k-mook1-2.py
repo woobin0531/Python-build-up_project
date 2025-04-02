49feb15cefc7bbfad50815ce28c93e149ae70030
woobin.jeong <cdevfrr@naver.com>
2025-04-02 12:51:07 +0900

    변경

--- k-mook1-2.py
+++ k-mook1-2.py
@@ -1,3 +1,6 @@
+#다음 문자열에서 "Python"이라는 단어가 몇 번 나오는지 세고,
+# 모든 "Python"을 "Java"로 변경한 후 리스트로 나누어 출력하세요.
+
 sentence = "I love Python. Python is fun. Python is powerful!"
 a = sentence.count("Python")  #단어 갯수 세기
 b = sentence.replace("Python", "java") ## "Python"을 "Java"로 변경
