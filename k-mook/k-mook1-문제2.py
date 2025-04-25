f52fab574b9245b03a263fe505fcb6d419ab3d20
woobin.jeong <cdevfrr@naver.com>
2025-04-25 15:58:54 +0900

    변경

--- k-mook/k-mook1-문제2.py
+++ k-mook/k-mook1-문제2.py
@@ -1,8 +1,9 @@
-#다음 문자열에서 "Python"이라는 단어가 몇 번 나오는지 세고,
-# 모든 "Python"을 "Java"로 변경한 후 리스트로 나누어 출력하세요.
-
-sentence = "I love Python. Python is fun. Python is powerful!"
-a = sentence.count("Python")  #단어 갯수 세기
-b = sentence.replace("Python", "java") ## "Python"을 "Java"로 변경
-c = b.split(".")  # 마침표(".")를 기준으로 문장을 나누어 리스트로 변환
-print(c)
+# 가장 긴 문자열 찾기
+# 아래 리스트에서 가장 길이가 긴 문자열을 출력하세요.
+words = ["dog", "elephant", "cat", "hippopotamus", "lion"]
+# 출력 예시: "hippopotamus"
+most_list = []
+for i in words:
+  if len(i) > len(most_list):
+    most_list = i
+print(most_list)
