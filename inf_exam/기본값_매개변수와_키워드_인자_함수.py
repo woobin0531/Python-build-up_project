51257c56b6e6848916c1aa058cf9f51c86169804
woobin.jeong <cdevfrr@naver.com>
2025-06-11 00:54:30 +0900

    변경

---
+++ inf_exam/기본값_매개변수와_키워드_인자_함수.py
@@ -0,0 +1,6 @@
+def woobin(num1, num2=2):
+    print('a =', num1, 'b =', num2)
+
+woobin(20)
+woobin(20, 3)
+woobin(num2=20, num1=3)
---
+++ inf_exam/짝수_판별_함수.py
@@ -0,0 +1,8 @@
+def fn(num):
+    if num % 2 == 0:
+        return 'Y'
+    else:
+        return 'N'
+
+a = fn(5)
+print(a)
